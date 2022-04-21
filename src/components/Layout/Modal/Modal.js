5959e4e7a27ad4a3c2dfd69c3fac9f72821d9893
rasolive <rasolive@hotmail.com>
2022-04-20 21:23:40 -0300

    fix Modal margin-top

--- src/components/Layout/Modal/Modal.js
+++ src/components/Layout/Modal/Modal.js
@@ -6,6 +6,7 @@ import ButtonGroup from "../ButtonGroup/ButtonGroup";
 
 export default function Modal(props) {
 	const Dialog = styled.dialog`
+		margin-top: 100px;
 		position: absolute;
 		flex-direction: column;
 		align-items: center;
@@ -16,7 +17,7 @@ export default function Modal(props) {
 		box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 		border-radius: 20px;
 		border: 0;
-		z-index: 2;
+		z-index: 1000;
 	`;
 
 	const Title = styled.span`
