9d6f7f20c02180a1abedef07c48e882ca1d12b34
rasolive <rasolive@hotmail.com>
2022-07-13 22:10:32 -0300

    Updates

---
+++ precache-manifest.31f3946c57dc95db37dd4bba5712c62f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567f71006ca1815635fd120a8792e6ed",
+    "url": "/your-app-name/index.html"
+  },
+  {
+    "revision": "6945d837695e22c78290",
+    "url": "/your-app-name/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "31fb941b32c672441f7e",
+    "url": "/your-app-name/static/css/main.f704e655.chunk.css"
+  },
+  {
+    "revision": "6945d837695e22c78290",
+    "url": "/your-app-name/static/js/2.a532ff15.chunk.js"
+  },
+  {
+    "revision": "d646726817a0096a943cf0c314b1edec",
+    "url": "/your-app-name/static/js/2.a532ff15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fb941b32c672441f7e",
+    "url": "/your-app-name/static/js/main.c96ac912.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/your-app-name/static/js/main.c96ac912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f222f738bacb6346139f",
+    "url": "/your-app-name/static/js/runtime-main.68cf153f.js"
+  }
+]);
